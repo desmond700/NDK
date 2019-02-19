254e9238f4d4795d836d360b0c7db34e29206581
desmond700 <dwdeveloper4@gmail.com>
2019-02-19 10:04:08 -0500

    initial commit

---
+++ app/src/main/cpp/Store.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Desmond on 2018-09-12.
+//
+
+#include "Store.h"
---
+++ app/src/main/cpp/Store.h
@@ -0,0 +1,14 @@
+//
+// Created by Desmond on 2018-09-12.
+//
+
+#ifndef NDKTEST_STORE_H
+#define NDKTEST_STORE_H
+
+
+class Store {
+
+};
+
+
+#endif //NDKTEST_STORE_H
